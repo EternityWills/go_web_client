d18f272a11d0fec643d2785244d2e3b13e31af2c
ATX_wuying <zzj18344152897@gamil.com>
2023-08-09 22:13:54 +0800

    0809

--- src/App.vue
+++ src/App.vue
@@ -15,8 +15,26 @@ let show: boolean = true
     {{ lpk('Type', { index: 1 }) }}
     <Icon icon="icon-time" />
     <van-button type="primary">主要按钮</van-button>
-    <van-calendar v-model:show="show" />
+    <!-- <van-calendar v-model:show="show" /> -->
+    <div class="theme">
+      <div class="theme-item" v-for="index in 3">{{ index }}</div>
+    </div>
   </div>
 </template>
 
-<style scoped></style>
+<style lang="scss" scoped>
+.theme {
+  display: flex;
+  justify-content: space-between;
+
+  .theme-item {
+    margin: 10px 20px;
+    width: 30%;
+    padding: 30px 0;
+    text-align: center;
+    @include fontSize();
+    @include bg();
+    @include color('big-size');
+  }
+}
+</style>
